3bcc61ab2c7efcd3368a541adacf73200aca3637
Ivar Framnes <ivar.william.kvale.framnes@nav.no>
2024-10-10 15:51:21 +0200

    fikse fikse

--- macros/row_access_policy_apply.sql
+++ macros/row_access_policy_apply.sql
@@ -1,10 +1,6 @@
 {% macro apply_row_access_policy(policy, using) %}
     {%- set materialization = config.get("materialized") -%}
     {% if materialization != "view" %} {%- set materialization = "table" -%} {% endif %}
-    {% set unset_policy_sql %}
-        alter {{materialization}} {{ this }} drop row access policy;  
-    {% endset %}
-    {% do run_query(unset_policy_sql) %}
 
     alter {{materialization}} {{ this }}
     {% if var("policy_db") is defined %}
