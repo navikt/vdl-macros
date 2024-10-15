30b3e07acd9d5d58ac831048e753f6c83b8af14f
Patrick Pedersen <patrick.pedersen@nav.no>
2024-10-15 16:02:09 +0200

    feat: create_alert macro
    This macro is meant to standardise how we create alerts.

---
+++ macros/create_alert.sql
@@ -0,0 +1,65 @@
+{% macro create_alert(
+    name,
+    message,
+    query,
+    integration,
+    warehouse="regnskap_streamer",
+    schedule="using cron 0-59/10 6-19 * * * Europe/Oslo",
+    database=target.database,
+    schema=var("alert_schema")
+) %}
+    {% set slack_integrations = {
+        "slack_test": "slack_test_webhook_int",
+        "slack_alert": "slack_alert_webhook_int",
+        "slack_info": "slack_info_webhook_int",
+    } %}
+    {% set warehouses = [
+        "serverless",
+        "regnskap_streamer",
+    ] %}
+
+    {% if integration not in slack_integrations.keys() %}
+        {{
+            exceptions.raise_compiler_error(
+                "Invalid `integration`. Got: "
+                ~ integration
+                ~ ".\nAvailable integrations: "
+                ~ slack_integrations
+            )
+        }}
+    {% endif %}
+    {% if warehouse not in warehouses %}
+        {{
+            exceptions.raise_compiler_error(
+                "Invalid `warehouse`. Got: "
+                ~ warehouse
+                ~ ".\nAvailable warehouses: "
+                ~ warehouses
+            )
+        }}
+    {% endif %}
+
+    {% set sql %}
+        create or replace alert {{ database }}.{{ schema }}.{{ name }}
+        schedule = '{{ schedule }}'
+        {% if warehouse != "serverless" %}
+            warehouse = '{{ warehouse }}'
+        {% endif %}
+        if(exists ({{ query }}))
+        then
+            call system$send_snowflake_notification(
+                snowflake.notification.text_plain(
+                    snowflake.notification.sanitize_webhook_content('{{ message }}')
+                ),
+                snowflake.notification.integration('{{ slack_integrations[integration] }}')
+            );
+    {% endset %}
+
+    {% do run_query(sql) %}
+    {% set resume %}
+      alter alert {{ database }}.{{ schema }}.{{ name }} resume;
+    {% endset %}
+    {% do run_query(resume) %}
+
+    {{ log("Alert " ~ name ~ " is created successfully", info=True) }}
+{% endmacro %}
