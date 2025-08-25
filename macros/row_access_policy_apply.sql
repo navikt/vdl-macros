8feea53576d5af1ee2f408aa381c7c5b98b6c3ce
Patrick Pedersen <patrick.pedersen@nav.no>
2025-08-25 14:58:46 +0200

    Endre fra å bruke variabler til parametere i row_access_policy_apply

--- macros/row_access_policy_apply.sql
+++ macros/row_access_policy_apply.sql
@@ -1,13 +1,9 @@
-{% macro apply_row_access_policy(policy, using) %}
+{% macro apply_row_access_policy(policy, using, database=this.database, schema="policies") %}
     {%- set materialization = config.get("materialized") -%}
     {% if materialization != "view" %} {%- set materialization = "table" -%} {% endif %}
 
-    alter {{materialization}} {{ this }}
-    {% if var("policy_db") is defined %}
-        add row access policy {{ var("policy_db") }}.{{ var("policy_schema") }}.{{ policy }}
-    {% else %}
-        add row access policy {{ this.database }}.{{ var("policy_schema") }}.{{ policy }}
-    {% endif %}
+    alter {{ materialization }} {{ this }}
+    add row access policy {{ database }}.{{ schema }}.{{ policy }}
     on (
     {%- for arg in using %}
     {{ arg }}{% if not loop.last %},{% endif %}
