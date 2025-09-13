98c7b6c763e29616f10bdf0868554e1f759e8467
Patrick Pedersen <patrick.pedersen@nav.no>
2025-09-13 15:36:14 +0200

    refactor: test models

---
+++ test_project/models/test_scd2_src.sql
@@ -0,0 +1,7 @@
+select
+    1 as unique_key,
+    1 as entity_key,
+    '2024-01-01 00:00:00'::timestamp as loaded_at,
+    '2024-01-01 01:00:00'::timestamp as updated_at,
+    '2024-01-01 01:00:00'::timestamp as created_at,
+    null::timestamp as deleted_at
